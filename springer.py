d70098b577662b5f18d7a3c7ccad5a6f5d3ffc46
Rebe Arteta <rebkartt@gmail.com>
2016-04-20 16:52:15 -0400

    Nuevo archivo abstract Springer

--- springer.py
+++ springer.py
@@ -17,53 +17,35 @@ def get_abstract_springer(url):
 
 		# Obtenemos todos los divs donde estan las entradas
 		entradas = html.find_all('section',{'class':'Abstract'})
-		# entradas = entradas[0].find_all('p', {'class' : 'Para'})
-		# abstract = ""
-		# for parrafo in entradas:
-		# 	abstract = abstract + parrafo.getText()
+		
 		try:
 			abstract = entradas[0].find('p', {'class' : 'Para'}).getText()
-			#print abstract.encode("utf-8")
-			return abstract
+			return abstract.encode("utf-8")
 		except:
 			return "--Abstract not found--"
 	else:
 		print ("Status Code %d") %statusCode
 
-#def write_csv_springer(abstracts, row):
 def write_csv_springer(abstracts):
+	file1 = open("SearchResults.csv", "rb")
+	reader = csv.reader(file1)
 	new_rows_list = []
 	i = 0
-	with open('SearchResults.csv') as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			new_row = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], abstracts[i]]
-			new_rows_list.append(new_row)
-			i = i + 1
-			if i > 3:
-				break
+	for row in reader:
+		#if row[8] == 'URL':
+		new_row = [row[0], row[1], row[2], row[3],row[4],row[5],row[6],row[7],row[8],row[9],abstracts[i]]
+		new_rows_list.append(new_row)
+		i = i + 1
+		if i > 150:
+		 	break
+	file1.close()   # <---IMPORTANT
 
-	with open('springer2.csv', 'w', newline='') as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerows(new_rows_list)				
-	# for row in reader:
-	# 	new_row = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], abstracts[i]]
-	# 	new_rows_list.append(new_row)
-	# 	i = i + 1
-	# 	if i > 149:
-	# 		break
-	# file1.close()   # <---IMPORTANT
-	# for abstract in abstracts:
-	# 	new_row = [abstract]
-	# 	new_rows_list.append(new_row)
 	# Do the writing
-	#new_row = [row["Item Title"], row["Publication Title"], row["Book Series Title"], row["Journal Volume"], row["Journal Issue"], row["Item DOI"], row["Authors"], row["Publication Year"], row["URL"], row["Content Type"], abstracts]
-	# new_rows_list.append(new_row)
-	# file2 = open('springer2.csv', 'w', newline='')
-	# writer = csv.writer(file2)
-	# writer.writerows(new_rows_list)
+	file2 = open("springer3.csv", "wb")
+	writer = csv.writer(file2)
+	writer.writerows(new_rows_list)
+	file2.close()				
 
-	# file2.close()
 
 
 # =========================================================================================
@@ -73,12 +55,13 @@ with open('SearchResults.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	i = 0
 	for row in reader:
+		if i > 150:
+			break
 		abstract = get_abstract_springer(row["URL"])
 		#write_csv_springer(abstract, row)
 		springer_abstract.append(abstract)
-		i = i +1
-		if i > 3:
-			break
+		i = i + 1
+
 
 write_csv_springer(springer_abstract)
 
