a057a4cd3b383f15396c1b6797aefe7bb904e1e3
Marcelo <marce.mmad@gmail.com>
2016-04-21 13:46:21 -0400

    agregado web of science

--- resultados/Lector.py
+++ resultados/Lector.py
@@ -2,6 +2,9 @@
 
 import csv
 
+from bs4 import BeautifulSoup
+import requests
+
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import homogeneize_latex_encoding
@@ -87,7 +90,60 @@ def read_taylor():
 				print "\n\n"
 
 
+def read_wos():
+	new_rows = []
+	# files = ["results_1-50", "results_51-100", "results_101-150", "results_151-200", "results_201-250", "results_251-300", "results_301-315"]
+	files = ["results_1-50"]
+
+	for file in files:
+		html =  BeautifulSoup(open("Web_of_Science/" + file), "lxml")
+		# Pasamos el contenido HTML de la web a un objeto BeautifulSoup()
+		# html = BeautifulSoup("SAGE_final_results.htm", 'lxml')
+		# Obtenemos todos los divs donde estan las entradas
+		# texto = html.getText()
+		# lineas = texto.split("Record")
+		# for linea in lineas:
+		# 	print linea.encode("utf-8")
+		# 	print "-------------------------------------------------------"
+		# print len(lineas)
+		entradas = html.find_all('table')
+		for entrada in entradas:
+			band = 0
+			campos = entrada.find_all('td')
+			for campo in campos:
+				titulo_campo = campo.find('b')
+				if titulo_campo:
+
+					if titulo_campo.string == "Title:":
+						title = campo.getText().replace("Title:", "").replace("\n", "")
+						band = band + 1
+						# print "TITLE= " + title
+					if titulo_campo.string == "By:":
+						authors = campo.getText().replace("By:", "").replace("\n", "")
+						band =  band + 1
+						# print "AUTHORS= " + authors
+					if titulo_campo.string == "Abstract:":
+						abstract = campo.getText().replace("Abstract:", "").replace("\n", "")
+						band = band + 1
+						# print "ABSTRACT= " + abstract
+					
+
+					# print title
+					# print authors
+					# print abstract
+			if band == 3:
+				url = "No se como poner el de Web_of_Science"
+				source = "Web_of_Science"
+				print "TITLE= " + title
+				print "AUTHORS= " + authors
+				print "ABSTRACT= " + abstract
+				new_rows.append([title, authors, source, url, abstract])
+			print "-------------------------------------------------------------------------------------"
+
+		
+
 # read_acm()
 # read_springer()
-read_wiley()
+# read_wiley()
 # read_taylor()
+read_wos()
